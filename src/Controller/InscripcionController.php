3b44d0b1eadbeb9e5b8070da9a2e417db4693bc7
Hectornauta <hemilianop13@ca.frre.utn.edu.ar>
2018-09-08 13:12:50 -0300

    Beta
    should work now.

--- src/Controller/InscripcionController.php
+++ src/Controller/InscripcionController.php
@@ -8,4 +8,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class InscripcionController extends AdminController
 {
+    public function persistEntity($entity)
+    {
+        parent::persistEntity($entity);
+        $id = $entity->getAlumno()->getId();
+        $monto = $entity->getCuota();
+
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $qb = $this->em->createQueryBuilder();
+        $qb->update('App\Entity\Alumno','a')
+            ->set('a.balance','a.balance - '.(string)$monto)
+            ->where('a.id = '.(string)$id);
+        return $qb->getQuery()->getResult();
+    }
 }
