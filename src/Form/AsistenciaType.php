01d90f4028bb83c4bb2cf03e79b67ffc7fc6f443
puerco-potter <albiach92@hotmail.com>
2018-09-08 11:53:21 -0300

    form asistencia funciona

--- src/Form/AsistenciaType.php
+++ src/Form/AsistenciaType.php
@@ -14,17 +14,24 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Repository\InscripcionRepository;
 
 class AsistenciaType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $id = $options["id"];
         $builder
             ->add('fecha', DateType::class)
             ->add('hora', TimeType::class)
             ->add('inscripcion',EntityType::class, array(
     // looks for choices from this entity
     'class' => Inscripcion::class,
+    // filtra la entidad
+    'query_builder' => function (InscripcionRepository $er)  use($id) {
+        return $er->createQueryBuilder('u')
+                ->where('u.Alumno = '. $id);
+    },
     'choice_label' => function (Inscripcion $inscripcion = null) {
         return null === $inscripcion ? '': $inscripcion->conseguir_asistencia();
     },
@@ -37,6 +44,7 @@ class AsistenciaType extends AbstractType
 
     public function configureOptions(OptionsResolver $resolver)
     {
+        $resolver->setRequired('id');
         $resolver->setDefaults(array(
             'data_class' => AsistenciaAlumno::class,
         ));
