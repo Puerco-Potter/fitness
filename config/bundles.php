527fdcc073053f99772cb1cdf65ac9c3b8ea9543
puerco-potter <albiach92@hotmail.com>
2018-09-08 10:13:58 -0300

    sin la basura de autocomplete

--- config/bundles.php
+++ config/bundles.php
@@ -15,5 +15,4 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
-    PUGX\AutocompleterBundle\PUGXAutocompleterBundle::class => ['all' => true],
 ];
