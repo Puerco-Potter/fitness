454c8957cc9ce6ac711b6d7fcef4c92f5cdc88e7
Hectornauta <hemilianop13@ca.frre.utn.edu.ar>
2018-10-15 21:46:20 -0300

    Anular movimiento ahora anda posta
    gg izi

--- src/Controller/MovimientoController.php
+++ src/Controller/MovimientoController.php
@@ -15,5 +15,19 @@ class MovimientoController extends AdminController
         #$entityManager->flush();
     }
     
+    public function anularAction()
+    {
+        $id = $this->request->query->get('id');
+        $entity = $this->em->getRepository(Movimiento::class)->find($id);
+        $entity->setAnulado(TRUE);
+        $this->em->flush();
+
+        return $this->redirectToRoute('easyadmin', array(
+            'action' => 'list',
+            'entity' => $this->request->query->get('entity'),
+        ));
+
+    }
+    
 }
 ?>
