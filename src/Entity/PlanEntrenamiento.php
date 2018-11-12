1c307aec6cac888868ed969cb63644b798b9a192
puerco-potter <albiach92@hotmail.com>
2018-11-12 17:08:10 -0300

    planes mejor formato, que viejas chotas

--- src/Entity/PlanEntrenamiento.php
+++ src/Entity/PlanEntrenamiento.php
@@ -41,6 +41,36 @@ class PlanEntrenamiento
      */
     private $lineas;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $dias1;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $dias2;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $dias3;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $dias4;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $dias5;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $dias6;
+
     public function __construct()
     {
         $this->rutinas = new ArrayCollection();
@@ -160,6 +190,78 @@ class PlanEntrenamiento
                 $linea->setPlanEntrenamiento(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getDias1(): ?string
+    {
+        return $this->dias1;
+    }
+
+    public function setDias1(?string $dias1): self
+    {
+        $this->dias1 = $dias1;
+
+        return $this;
+    }
+
+    public function getDias2(): ?string
+    {
+        return $this->dias2;
+    }
+
+    public function setDias2(?string $dias2): self
+    {
+        $this->dias2 = $dias2;
+
+        return $this;
+    }
+
+    public function getDias3(): ?string
+    {
+        return $this->dias3;
+    }
+
+    public function setDias3(?string $dias3): self
+    {
+        $this->dias3 = $dias3;
+
+        return $this;
+    }
+
+    public function getDias4(): ?string
+    {
+        return $this->dias4;
+    }
+
+    public function setDias4(?string $dias4): self
+    {
+        $this->dias4 = $dias4;
+
+        return $this;
+    }
+
+    public function getDias5(): ?string
+    {
+        return $this->dias5;
+    }
+
+    public function setDias5(?string $dias5): self
+    {
+        $this->dias5 = $dias5;
+
+        return $this;
+    }
+
+    public function getDias6(): ?string
+    {
+        return $this->dias6;
+    }
+
+    public function setDias6(?string $dias6): self
+    {
+        $this->dias6 = $dias6;
 
         return $this;
     }
